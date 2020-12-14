de53c14ff94b2997a359581401368f7aa10741d5
bideeen <b.abideen@yahoo.com>
2020-12-13 17:45:24 -0700

    time tracker

---
+++ time_tracker.py
@@ -0,0 +1,16 @@
+# Import time library
+import time
+# Start time
+start_time = time.time()
+
+# The main function to perform some calculation
+def main():
+    return list(i%2 for i in range(31))
+
+# call the fubction
+main()
+# the time the functin finished execution
+end_time = time.time()
+# The total time 
+totalTime = end_time - start_time
+print(f'Time taken to execute code= {totalTime}')
