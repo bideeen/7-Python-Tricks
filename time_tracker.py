d2e4332ef81a6c2db6317d4f064b0f4029cb1f8d
bideeen <b.abideen@yahoo.com>
2020-12-14 19:21:30 -0700

    TIME UPDATED

--- time_tracker.py
+++ time_tracker.py
@@ -8,9 +8,11 @@ def main():
     return list(i%2 for i in range(31))
 
 # call the fubction
-main()
-# the time the functin finished execution
-end_time = time.time()
-# The total time 
-totalTime = end_time - start_time
-print(f'Time taken to execute code= {totalTime}')
+if __name__ == "__main__":
+    
+    main()
+    # the time the functin finished execution
+    end_time = time.time()
+    # The total time 
+    totalTime = end_time - start_time
+    print(f'Time taken to execute code= {totalTime}')
