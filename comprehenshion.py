1bbb1e13c407b4682893094c07946ae4ad383284
bideeen <b.abideen@yahoo.com>
2020-12-14 19:57:22 -0700

    updated comprehension 1.0

--- comprehenshion.py
+++ comprehenshion.py
@@ -1,8 +1,9 @@
+# First form of list comprehension
 x = [i for i in range(20)]
 
 y = [j for j in x if j%2 == 0]
 
-# or more
+# Second form of list comprehension
 
 z = [k for k in range(20) if k%2 == 0]
 
